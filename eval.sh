47bbfbc03e7bd5095ae0ad4b8ffb0bedba26e028
jxbbb <jinbu18@mails.ucas.ac.cn>
2024-06-01 10:48:53 +0000

    tod3cap netweork init with bevformer

---
+++ eval.sh
@@ -0,0 +1,4 @@
+# python evaluate_utils/get_text_results.py --result_path test/bevformer_base/box_eval_e10/pts_bbox/results_nusc.json
+python evaluate_utils/get_text_results.py --result_path test/bevformer_small/caption_eval/pts_bbox/results_nusc.json
+# python evaluate_utils/get_text_results.py --result_path test/bevformer_tiny/train_all_from_zero_e24/pts_bbox/results_nusc.json
+# python evaluate_utils/get_text_results.py --result_path cam+lidar.pkl
