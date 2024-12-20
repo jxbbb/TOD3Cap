30a65a1118fe3a370534060a2e2c22e613f82dd5
ucaszyp <zhengyupeng18@mails.ucas.ac.cn>
2024-12-20 11:22:34 +0800

    init temporal fusion

--- tod3cap_tf/eval.sh
+++
@@ -1,1 +0,0 @@
-PYTHONPATH="." python evaluate_utils/get_text_results.py --result_path /data/zyp/Driving/TOD3Cap/tod3cap_camera/test/bevformer_tiny_test/Wed_Dec_18_03_16_12_2024/pts_bbox/results_nusc.json
